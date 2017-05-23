f5d3a9f1b5e1ad79b482182fd5642df285db0bdc
Vikram Sreekanti <vsreekanti@gmail.com>
2017-05-23 15:44:06 -0700

    add mnist nn

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-import mnist_softmax
+import mnist_nn
+import os
+import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
-import os
-import numpy as np
 
 data_dir = '/tmp/tensorflow/mnist/input_data'
 
@@ -12,33 +12,15 @@ data_dir = '/tmp/tensorflow/mnist/input_data'
 mnist = input_data.read_data_sets(data_dir, one_hot=True)
 
 # Train model
-(save_path, model_name) = mnist_softmax.train(data_dir)
-
+(save_path, model_name) = mnist_nn.train(data_dir)
+# save_path = "mnist.model"
 
 with tf.Session() as sess:
   saver = tf.train.import_meta_graph(os.path.join(save_path, model_name + ".meta"))
   saver.restore(sess, os.path.join(save_path, model_name))
 
-  preds = np.argmax(sess.run('prediction:0', feed_dict={'x:0':mnist.test.images}), axis=1)
+  preds = np.argmax(sess.run('softmax_logits:0', feed_dict={'x:0':mnist.test.images, 'keep_prob:0':1.0}), axis=1)
   labels = np.argmax(mnist.test.labels, axis=1)
 
   accuracy = float(np.sum(preds == labels))/float(len(preds))
   print("Accuracy:\n%g" % accuracy)
-
-
-# # W = tf.Variable(tf.zeros([784, 10]))
-# # b = tf.Variable(tf.zeros([10]))
-# # saver = tf.train.Saver({'W': W, 'b': b})
-# #
-# # sess = tf.InteractiveSession()
-# # saver.restore(sess, "./mnist.model")
-# #
-# # x = tf.placeholder(tf.float32, [None, 784])
-# # y_ = tf.placeholder(tf.float32, [None, 10])
-# # y = tf.matmul(x, W) + b
-#
-# # Test trained model
-# correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-# print("Test accuracy is:")
-# print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
